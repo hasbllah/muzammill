32da8db5de1756f9d89cd252429974af76357b53
Muzammill <101366851+hasbllah@users.noreply.github.com>
2022-03-11 06:52:46 +0800

    Update muzammill.py

--- muzammill.py
+++ muzammill.py
@@ -1,1 +1,7 @@
-
+$ pkg update && pkg upgrade
+$ pkg install python2
+$ pkg install git
+$ git clone https://github.com/hasbllah/muzammill
+$ cd muzammill
+$ pip2 install muzammill.h
+$ python2 muzammill.py
